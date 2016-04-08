c642ae361a66ce1c283d189846865500cbd1585b
Alice Bevan–McGregor <alice@gothcandy.com>
2016-04-08 05:58:51 -0400

    Google Analytics.

--- web/component/page/render.py
+++ web/component/page/render.py
@@ -2,9 +2,8 @@
 
 : import traceback
 
+
 : log = __import__('logging').getLogger(__name__)
-
-
 
 
 : def render_block context, page, block
@@ -81,4 +80,19 @@
 
 </article>
 
+	: if not __debug__ and 'ua' in context.croot.properties
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', '${context.croot.properties.ua}', 'auto');
+ga('send', 'pageview');
+</script>
+	: end
+
+	: end
+
+	: flush
 : end
